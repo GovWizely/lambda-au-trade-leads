3887b1d3601881bdad9a031dd0057f68b65724cd
Donna Hogan <ddhogan@users.noreply.github.com>
2020-05-05 21:43:01 -0400

    [MDS-421] Update css selectors used by BeautifulSoup to match restructured endpoint
    - Include previous set of selectors since the endpoint has reverted previously between these sets

--- test_service.py
+++ test_service.py
@@ -12,49 +12,53 @@ def test_get_trade_lead_links():
 
     """
     entries = list(get_trade_lead_links())
-    assert len(entries) == 101
+    assert len(entries) == 102
     assert entries[0] == {
-        "atm_id": "SPC 3773",
-        "agency": "Australian Taxation Office",
-        "category": "80131500 - Lease and rental of property or building",
-        "close_date_time": "2020-04-15T10:00:00+10:00",
-        "publish_date": "Mon, 16 Mar 2020 13:00:00 GMT",
-        "location": "ACT",
-        "atm_type": "Expression of Interest",
-        "multi_agency_access": "No",
+        "atm_id": "RFT 1-2020",
+        "agency": "Australian Federal Police",
+        "category": "43230000 - Software",
+        "close_date_time": "2020-05-06T11:00:00+10:00",
+        "publish_date": "Thu, 13 Feb 2020 13:00:00 GMT",
+        "location": "Canberra",
+        "atm_type": "Request for Tender",
+        "multi_agency_access": "Yes",
+        "multi_agency_access_type": "All Agencies",
         "panel_arrangement": "No",
-        "multistage": "Yes",
-        "multistage_criteria": "As defined in the Request documentation.",
-        "description": "Colliers International, on behalf of the Australian Taxation Office ("
-                       "ATO), invites potential suppliers to submit a submission for a "
-                       "multi-stage procurement for the provision of leased office accommodation "
-                       "for:  The location parameters within Canberra Net Lettable Area (NLA) "
-                       "of\xa0 approximately 32,000m2 Lease term of ten (10) years with two (2) "
-                       "by five (5) year options.",
-        "other_instructions": "The Request documentation for this procurement opportunity can be "
-                              "obtained from Colliers Tenderlink portal at "
-                              "https://www.tenderlink.com/colliersinternational/. Addendum to the "
-                              "Request documentation will also be made available via Tenderlink. "
-                              "Tenderers are required to log in to Tenderlink and collect "
-                              "addendum as notified.  The ATO will accept no responsibility if a "
-                              "Tenderer fails to become aware of any addendum which would have "
-                              "been apparent from a visit to the Tenderlink page for this "
-                              "procurement activity.",
-        "conditions_for_participation": "The Request documentation for this procurement activity "
-                                        "contains Conditions of Participation and Minimum Content "
-                                        "and Format Requirements. Tenderers are reminded that "
-                                        "subject to clarification for unintentional errors or "
-                                        "errors in form, Submissions that do not meet the "
-                                        "Conditions of Participation and Minimum Content and "
-                                        "Format Requirements will be excluded from further "
-                                        "consideration.",
-        "timeframe_for_delivery": "As defined in the Request documentation",
-        "address_for_lodgement": "As defined in the Request documentation.",
-        "tender_url": "https://www.tenders.gov.au/Atm/Show/4404e5d3-13ab-43ce-94bb-7e9c41947fe9",
-        "uuid": "s://www.tenders.gov.au/Atm/Show/4404e5d3-13ab-43ce-94bb-7e9c41947fe9",
-        "title": "Expression of Interest - Leased Office Accommodation - Canberra",
-        "contact_officer": "Contact Details",
-        "email": "WoAG.Leasing@colliers.com",
+        "multistage": "No",
+        "description": "The AFP seeks responses from suitably qualified and experienced Tenderer"
+                       "(s) for the provision of a software and support services solution that "
+                       "will enable the AFP to maintain an Organisational Health Record for all AFP"
+                       " Personnel.",
+        "other_instructions": "An industry briefing will be held at 11am on Wednesday 26/2/20.",
+        "conditions_for_participation": "The AFP will exclude a Tender from further consideration"
+                                        " if it considers that, at the time of lodgement of the "
+                                        "Tender, the Tenderer: (a)     does not exist as a legal "
+                                        "entity at the time of lodgement of the Tender; (b)      is"
+                                        " not solvent or financially viable; (c)      does not hold"
+                                        " an Authorisation necessary for it to be able to perform "
+                                        "the Services tendered for in accordance with applicable "
+                                        "laws; or (d)     does not:    (i)       hold a Valid and "
+                                        "Satisfactory Statement of Tax Record by the Closing Time; "
+                                        "or    (ii)      have a receipt demonstrating that a State"
+                                        "ment of Tax Record has been requested from the Australian "
+                                        "Taxation Office by the Closing Time, and holds a Valid and"
+                                        " Satisfactory Statement of Tax Record no later than 4 Busi"
+                                        "ness Days from the Closing Time; and    (iii)     does not"
+                                        " hold a Valid and Satisfactory Statement of Tax Record for"
+                                        " any first tier subcontractor that it proposes, as part of"
+                                        " its Tender, to engage to deliver the Services with an est"
+                                        "imated value of over $4 million (GST inclusive).",
+        "timeframe_for_delivery": "by September 2020",
+        "estimated_value_aud": "From to",
+        "address_for_lodgement": "Tenders must be lodged electronically via the AusTender, at "
+        "https://www.tenders.gov.au before the Closing Time and in accordance with the Tender lodge"
+        "ment procedures set out in this RFT and on AusTender.",
+        "addenda_available": "View Addenda",
+        "tender_url": "https://www.tenders.gov.au/Atm/Show/8d362bc7-2b95-4892-8b06-6e82f694788e",
+        "uuid": "s://www.tenders.gov.au/Atm/Show/8d362bc7-2b95-4892-8b06-6e82f694788e",
+        "title": "Provision of an Organisational Health Solution",
+        "contact_officer": "Heath Galer",
+        "email": "AFP-RFT@afp.gov.au"
     }
 
 
